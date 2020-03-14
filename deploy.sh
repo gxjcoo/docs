b6aba470198c1f259f06b0c71674fa9c38cce27e
gxjcoo <gxjcoo@gmail.com>
2020-03-14 20:57:38 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
+
+# 如果是发布到自定义域名
+# echo 'www.example.com/docs' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:gxjcoo/gxjcoo.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:gxjcoo/docs.git master:gh-pages
+
+
+# 提交代码至git仓库
+
+git pull 
+
+git add .
+
+git commit -m "$*"
+
+git push -u origin master
+
+cd -
