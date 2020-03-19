2c9ae32db29ccec5461fd93cf429796e1d562c5d
gxjcoo <gxjcoo@gmail.com>
2020-03-19 20:52:47 +0800

    自动化部署，本次提交无关键信息

--- docs/.vuepress/slide.js
+++ docs/.vuepress/slide.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       title: "前端基础",
       collapsable: false,
-      children: ["html", "css", "js", "es6+", "vue", "项目优化"]
+      children: ["http","html", "css", "js", "es6+", "vue", "项目优化"]
     }],
     "/article/interview/": [
     {
